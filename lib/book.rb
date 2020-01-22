1070a5820d409cddbfb6d770172278f8b58473f0
kkirby16 <kkirby16@my.whitworth.edu>
2020-01-21 17:20:08 -0800

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,12 +1,12 @@
 class Book
-  attr_accessor :author, :page_count, :genre 
+  attr_accessor :author, :page_count, :genre
   attr_reader :title
 
   def initialize(title)
     @title = title
-  end 
+  end
 
-  def turn_page 
+  def turn_page
     puts "Flipping the page...wow, you read fast!"
-  end 
-end 
+  end
+end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,47 +1,15 @@
+require "pry"
+
 class Shoe
+  attr_accessor :brand, :color, :size, :material, :condition
+  attr_reader :brand
 
-def initialize(brand)
-    @brand = brand 
-end 
+  def initialize(brand)
+    @brand = brand
+  end
 
-def brand 
-    @brand
-end 
-
-def color=(color)
-    @color = color
-end 
-
-def color 
-    @color 
-end 
-
-def size=(size)
-    @size = size 
-end 
-
-def size 
-    @size 
-end 
-
-def material=(material)
-    @material = material 
-end 
-
-def material 
-    @material 
-end 
-
-def condition=(condition)
-    @condition = condition
-end 
-
-def condition
-    @condition
-end 
-
-def cobble 
-    puts "Your shoe is as good as new!"
+  def cobble
     @condition = "new"
-end 
-end 
+    puts "Your shoe is as good as new!"
+  end
+end
